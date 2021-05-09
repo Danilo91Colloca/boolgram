8824484f83c8f48bb2aec30db5f46778ec29a789
Danilo91Colloca <danilo.colloca91@gmail.com>
2021-05-09 23:46:22 +0200

    box stories draft

--- src/App.vue
+++ src/App.vue
@@ -24,84 +24,86 @@
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-#app {
-  min-width: 350px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    min-width: 350px;
+    position: relative;
+  }
 
-#nav {
-  // background-color: red; //TEMP
-  border-top: 2px solid lightgray;
-  border-bottom: 2px solid lightgray;
-  height: 80px;
-  width: 100vw;
-  max-width: 100%;
-  .nav-content {
-    width: 1200px;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    .nav-logo {
+  #nav {
+    position: fixed;
+    border-top: 2px solid lightgray;
+    border-bottom: 2px solid lightgray;
+    height: 80px;
+    width: 100vw;
+    max-width: 100%;
+    background-color: #fff;
+    .nav-content {
+      width: 1200px;
       height: 100%;
-      width: 20%;
+      margin: 0 auto;
       display: flex;
-      justify-items: center;
-      align-items: center;
-      img {
-        width: 140px;
-      }
-      &:hover {
-        cursor: pointer;
-      }
-    }
-    .nav-search {
-      height: 100%;
-      width: 40%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      input {
-        width: 50%;
-        line-height: 20px;
-        text-align: center;
-        &:focus {
-          outline: 1px solid black;
-        }
-      }
-    }
-    .nav-likeHomeUser {
-      height: 100%;
-      width: 20%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .icon {
-        font-size: 18px;
-        margin-left: 25px;
-        &:hover {
-          cursor: pointer;
-        }
-      }
-      .img-profile {
-        width: 38px;
-        overflow: hidden;
-        border-radius: 50%;
-        margin-left: 25px;
+      justify-content: space-between;
+      .nav-logo {
+        height: 100%;
+        width: 20%;
+        display: flex;
+        justify-items: center;
+        align-items: center;
         img {
-          width: 100%;
-          border-radius: 50%;
-          border: 2px solid lightgray;
+          width: 140px;
         }
         &:hover {
           cursor: pointer;
+        }
+      }
+      .nav-search {
+        height: 100%;
+        width: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        input {
+          width: 50%;
+          line-height: 20px;
+          text-align: center;
+          &:focus {
+            outline: 1px solid black;
+          }
+        }
+      }
+      .nav-likeHomeUser {
+        height: 100%;
+        width: 20%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          font-size: 18px;
+          margin-left: 25px;
+          &:hover {
+            cursor: pointer;
+          }
+        }
+        .img-profile {
+          width: 38px;
+          overflow: hidden;
+          border-radius: 50%;
+          margin-left: 25px;
+          img {
+            width: 100%;
+            border-radius: 50%;
+            border: 2px solid lightgray;
+          }
+          &:hover {
+            cursor: pointer;
+          }
         }
       }
     }
   }
-}
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,65 @@
 <template>
   <div class="main">
     <main class="wrapper-center">
+      <!-- LEFT SECTION -->
       <div class="main-left">
         <div class="stories-box">
-          <h1>stoires container</h1>
+          <!-- stories box -->
+          <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+          <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+           
+          <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+          <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+
+          <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+
+           <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
+
+           <div class="story-container">
+            <div class="container-imgUser-story">
+              <img src="../assets/profile.jpg" alt="">
+            </div>
+            <h5 class="user-name-story">ciccio_pippo91</h5>
+          </div>
         </div>
+
+        <!-- posts box -->
         <div class="posts-box">
           <Post msg="post's components" />
         </div>
       </div>
+
+      <!-- RIGHT SECTION -->
       <div class="main-right">
         <h2>profilo</h2>
         <h2>suggerimenti</h2>
@@ -21,7 +72,6 @@
 <script>
 // @ is an alias to /src
 import Post from "@/components/Post.vue";
-
 export default {
   name: "App",
   components: {
@@ -32,6 +82,8 @@ export default {
 
 <style lang="scss" scoped>
 .main {
+  position: absolute;
+  top: 80px;
   width: 100vw;
   max-width: 100%;
   height: 100%;
@@ -47,8 +99,37 @@ export default {
       background-color: green;
       .stories-box {
         width: 100%;
-        height: 200px;
-        background: blue; //TEMP
+        height: 180px;
+        background: #fff; 
+        display: flex;
+        border: 2px solid lightgray;
+        margin-top: 10px;
+        overflow-x: auto;
+        .story-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin: 0px 10px;
+            .container-imgUser-story {
+            width: 50px;
+            border-top: 2px solid rgb(214,11,120);
+            border-right: 2px solid rgb(214,11,120);
+            border-left:  2px solid gold;
+            border-bottom:  2px solid gold;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            padding: 2px;  
+            margin-bottom: 5px;        
+            img {
+              width: 100%;
+              border-radius: 50%;
+            }
+          }
+        }
+        
+
       }
     }
     .main-right {
