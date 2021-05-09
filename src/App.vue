94b317df84b314809201f7f3e70ef97dc23e0d8d
Danilo91Colloca <danilo.colloca91@gmail.com>
2021-05-09 21:43:52 +0200

    navbar advanced draft

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
   <div id="app">
     <div id="nav">
       <div class="nav-content">
+        <div class="nav-logo">
+          <img src="./assets/logo.png" alt="" />
+        </div>
 
-          <div class="nav-logo">
-              <img src="./assets/logo.png" alt="">
+        <div class="nav-search">
+          <input id="serach" type="text" placeholder="Cerca" />
+        </div>
+        <div class="nav-likeHomeUser">
+          <i class="far fa-heart icon"></i>
+          <i class="fas fa-home icon"></i>
+          <div class="img-profile">
+             <img src="./assets/profile.jpg" alt="">
           </div>
-          
-          <div class="nav-search">
-            <input id="serach" type="text" placeholder="Cerca">
-          </div>
-          <div class="nav-likeHomeUser">
-
-          </div>
+        </div>
       </div>
     </div>
+
     <router-view />
   </div>
 </template>
@@ -31,12 +35,11 @@
 
 #nav {
   // background-color: red; //TEMP
-  border-top: 2px solid lightgray; 
+  border-top: 2px solid lightgray;
   border-bottom: 2px solid lightgray;
   height: 80px;
   width: 100vw;
   max-width: 100%;
-
   .nav-content {
     width: 1200px;
     height: 100%;
@@ -45,17 +48,20 @@
     justify-content: space-between;
     .nav-logo {
       height: 100%;
-      width: 30%;
+      width: 20%;
       display: flex;
       justify-items: center;
       align-items: center;
       img {
         width: 140px;
       }
+      &:hover {
+        cursor: pointer;
+      }
     }
     .nav-search {
       height: 100%;
-      width: 30%;
+      width: 40%;
       display: flex;
       justify-content: center;
       align-items: center;
@@ -63,15 +69,39 @@
         width: 50%;
         line-height: 20px;
         text-align: center;
+        &:focus {
+          outline: 1px solid black;
+        }
       }
     }
     .nav-likeHomeUser {
       height: 100%;
-      width: 30%;
-      background-color: gray; //TEMP
+      width: 20%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        font-size: 18px;
+        margin-left: 25px;
+        &:hover {
+          cursor: pointer;
+        }
+      }
+      .img-profile {
+        width: 38px;
+        overflow: hidden;
+        border-radius: 50%;
+        margin-left: 25px;
+        img {
+          width: 100%;
+          border-radius: 50%;
+          border: 2px solid lightgray;
+        }
+        &:hover {
+          cursor: pointer;
+        }
+      }
     }
   }
-  
 }
-
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,17 +2,17 @@
   <div class="main">
     <main class="wrapper-center">
       <div class="main-left">
-          <div class="stories-box">
-              <h1>stoires container</h1>
-          </div>
-          <div class="posts-box">
-            <Post msg="post's components" />
-          </div>
+        <div class="stories-box">
+          <h1>stoires container</h1>
+        </div>
+        <div class="posts-box">
+          <Post msg="post's components" />
+        </div>
       </div>
       <div class="main-right">
-          <h2>profilo</h2>
-          <h2>suggerimenti</h2>
-          <h2>footer</h2>
+        <h2>profilo</h2>
+        <h2>suggerimenti</h2>
+        <h2>footer</h2>
       </div>
     </main>
   </div>
@@ -31,34 +31,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  
-  .main {
-    width: 100vw;
-    max-width: 100%;
-    height: 100%;
-    main.wrapper-center {
-      width: 1200px;
-      background-color: lightgray; //TEMP
-      height: calc(100vh - 80px); //TEMP
-      margin: 0 auto;
-      display: flex;
-      .main-left{
-        width: 50%;
-        height:100%;
-        background-color: green;
-        .stories-box {
-          width: 100%;
-          height: 200px;
-          background: blue; //TEMP
-        }
-      }
-      .main-right{
-        width: 50%;
-        height:100%;
-        background-color: yellow;
+.main {
+  width: 100vw;
+  max-width: 100%;
+  height: 100%;
+  main.wrapper-center {
+    width: 1200px;
+    background-color: lightgray; //TEMP
+    height: calc(100vh - 80px); //TEMP
+    margin: 0 auto;
+    display: flex;
+    .main-left {
+      width: 50%;
+      height: 100%;
+      background-color: green;
+      .stories-box {
+        width: 100%;
+        height: 200px;
+        background: blue; //TEMP
       }
     }
+    .main-right {
+      width: 50%;
+      height: 100%;
+      background-color: yellow;
+    }
   }
-  
-
+}
 </style>
