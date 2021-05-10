b88f2d6b54d12bf199a71ff7b07fddaf4af0fac8
Danilo91Colloca <danilo.colloca91@gmail.com>
2021-05-10 09:48:49 +0200

    create cartella con dile scss e import nelle sezioni/bozza sezione commenti

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="post">
+      <div class="post-author-box">
+        <div class="post-userData">
+          <div class="post-userImage">
+            <!-- <img src="" alt=""> -->
+
+          </div>
+          <h5>NAME AUTHOR</h5>
+        </div>
+        <div class="menu-post">
+          <i class="fas fa-ellipsis-h"></i>
+        </div>
+      </div>
+      <div class="post-img-box">
+        <img src="" alt="">
+        POST IMAGE
+      </div>
+      <div class="like-message-box">
+        <div class="icon-like-msg">
+          <i class="far fa-heart"></i>
+          <i class="far fa-comment"></i>
+        </div>
+        <div class="like-users">
+          <div class="img-first-user">
+            <img src="" alt="">
+          </div>
+          <h5>user name</h5>
+        </div>
+      </div>
+
   </div>
 </template>
 
@@ -8,12 +36,11 @@
 export default {
   name: "Post",
   props: {
-    msg: String,
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+  @import "../scss/_post";
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -11,40 +11,42 @@
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
+          <!-- TEMP -->
           <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
-           
+          <!-- TEMP --> 
           <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
+          <!-- TEMP -->
           <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
-
+            <!-- TEMP -->
           <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
-
+            <!-- TEMP -->
            <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
             </div>
             <h5 class="user-name-story">ciccio_pippo91</h5>
           </div>
-
+          <!-- TEMP -->
            <div class="story-container">
             <div class="container-imgUser-story">
               <img src="../assets/profile.jpg" alt="">
@@ -55,7 +57,7 @@
 
         <!-- posts box -->
         <div class="posts-box">
-          <Post msg="post's components" />
+          <Post />
         </div>
       </div>
 
@@ -81,62 +83,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.main {
-  position: absolute;
-  top: 80px;
-  width: 100vw;
-  max-width: 100%;
-  height: 100%;
-  main.wrapper-center {
-    width: 1200px;
-    background-color: lightgray; //TEMP
-    height: calc(100vh - 80px); //TEMP
-    margin: 0 auto;
-    display: flex;
-    .main-left {
-      width: 50%;
-      height: 100%;
-      background-color: green;
-      .stories-box {
-        width: 100%;
-        height: 180px;
-        background: #fff; 
-        display: flex;
-        border: 2px solid lightgray;
-        margin-top: 10px;
-        overflow-x: auto;
-        .story-container {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin: 0px 10px;
-            .container-imgUser-story {
-            width: 50px;
-            border-top: 2px solid rgb(214,11,120);
-            border-right: 2px solid rgb(214,11,120);
-            border-left:  2px solid gold;
-            border-bottom:  2px solid gold;
-            border-radius: 50%;
-            display: flex;
-            justify-content: center;
-            padding: 2px;  
-            margin-bottom: 5px;        
-            img {
-              width: 100%;
-              border-radius: 50%;
-            }
-          }
-        }
-        
-
-      }
-    }
-    .main-right {
-      width: 50%;
-      height: 100%;
-      background-color: yellow;
-    }
-  }
-}
+    @import "../scss/main";
 </style>
